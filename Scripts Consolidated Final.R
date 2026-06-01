6c881566dc17a40e19c81b44f7b6ec8171c18d22
Riku Matsumoto <rikumatsumoto@uw.edu>
2026-05-31 20:51:09 -0700

    New file for final version

---
+++ Scripts Consolidated Final.R
@@ -0,0 +1,30 @@
+#Final Version of our R script
+#Everything will be consolidated in here with documentation
+
+#install packages
+
+
+
+#libraries
+
+
+
+#Import Data
+
+
+
+#Script
+
+
+
+#Correlation Matrix
+
+
+#Scatter Plots
+
+
+
+#Linear Regression
+
+
+#Etc Etc
